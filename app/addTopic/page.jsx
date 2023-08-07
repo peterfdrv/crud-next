af65a3f700f39ad1746ae2fc908d511ad8e600f0
Fedorov Peter <fedorovpeter97@gmail.com>
2023-08-07 20:28:56 +0200

    created the addTopic page with styling

---
+++ app/addTopic/page.jsx
@@ -0,0 +1,20 @@
+export default function AddTopic() {
+  return (
+    <form className="flex flex-col gap-3">
+      <input
+        className="border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="Topic title"
+      />
+
+      <input
+        className="border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="Topic description"
+      />
+      <button className="bg-green-600 bold text-white py-3 px-6 w-fit">
+        Add topic
+      </button>
+    </form>
+  );
+}
