5c4cd502dfe074e69998940f6fc0cb813128853f
Fedorov Peter <fedorovpeter97@gmail.com>
2023-08-07 20:22:45 +0200

    page.jsx now returns the TopicsList

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,10 @@
 import TopicsList from '@/components/TopicsList';
 
 export default function Home() {
-  return <TopicsList />;
+  return (
+    <>
+      <TopicsList />
+      <TopicsList />
+    </>
+  );
 }
