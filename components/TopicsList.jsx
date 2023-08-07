449f223a7c5867a76a6eaaf786bf9fc394365c37
Fedorov Peter <fedorovpeter97@gmail.com>
2023-08-07 20:19:02 +0200

    Created topics lists styling

--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -5,15 +5,18 @@ import { HiPencilAlt } from 'react-icons/hi';
 export default function TopicsList() {
   return (
     <>
-      <div>
+      <div className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start">
         <div>
-          <h2>Topic Title</h2>
+          <h2 className="font-bold text-2xl">Topic Title</h2>
           <div>Topic Description</div>
         </div>
-        <RemoveBtn />
-        <Link href={'/editTopics/123'}>
-          <HiPencilAlt size={24} />
-        </Link>
+
+        <div className="flex gap-2">
+          <RemoveBtn />
+          <Link href={'/editTopics/123'}>
+            <HiPencilAlt size={24} />
+          </Link>
+        </div>
       </div>
     </>
   );
