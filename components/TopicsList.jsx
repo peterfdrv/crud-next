81978e1a95c88663de694d2d612fbf214210b2bb
Fedorov Peter <fedorovpeter97@gmail.com>
2023-08-07 19:54:37 +0200

    Created TopicList component

---
+++ components/TopicsList.jsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import RemoveBtn from './RemoveBtn';
+import { HiPencilAlt } from 'react-icons/hi';
+
+export default function TopicsList() {
+  return (
+    <>
+      <div>
+        <div>
+          <h2>Topic Title</h2>
+          <div>Topic Description</div>
+        </div>
+        <RemoveBtn />
+        <Link href={'/editTopics/123'}>
+          <HiPencilAlt size={24} />
+        </Link>
+      </div>
+    </>
+  );
+}
