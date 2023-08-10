962b453f609c06fcabf373275d2ac7b8da6a6c81
Fedorov Peter <fedorovpeter97@gmail.com>
2023-08-10 18:47:09 +0200

    getTopics function for fetching topics

--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -2,6 +2,21 @@ import Link from 'next/link';
 import RemoveBtn from './RemoveBtn';
 import { HiPencilAlt } from 'react-icons/hi';
 
+const getTopics = async () => {
+  try {
+    const res = await fetch('http://localhost:3000/api/topics', {
+      cache: 'no-store',
+    });
+
+    if (!res.ok) {
+      throw new Error('Failed to fetch topics');
+    }
+    return res.json();
+  } catch (error) {
+    console.log('Error loading topics', error);
+  }
+};
+
 export default function TopicsList() {
   return (
     <>
