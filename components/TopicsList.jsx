8747f2364948a082260bbecbee1433fd380d7ddd
Fedorov Peter <fedorovpeter97@gmail.com>
2023-08-10 18:51:54 +0200

    expanded TopicsList function to display the topics

--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -17,22 +17,29 @@ const getTopics = async () => {
   }
 };
 
-export default function TopicsList() {
+export default async function TopicsList() {
+  const { topics } = await getTopics();
+
   return (
     <>
-      <div className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start">
-        <div>
-          <h2 className="font-bold text-2xl">Topic Title</h2>
-          <div>Topic Description</div>
-        </div>
+      {topics.map((t) => (
+        <div
+          key={t._id}
+          className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start"
+        >
+          <div>
+            <h2 className="font-bold text-2xl">{t.title}</h2>
+            <div>{t.description}</div>
+          </div>
 
-        <div className="flex gap-2">
-          <RemoveBtn />
-          <Link href={'/editTopics/123'}>
-            <HiPencilAlt size={24} />
-          </Link>
+          <div className="flex gap-2">
+            <RemoveBtn />
+            <Link href={`/editTopics/${t._id}`}>
+              <HiPencilAlt size={24} />
+            </Link>
+          </div>
         </div>
-      </div>
+      ))}
     </>
   );
 }
