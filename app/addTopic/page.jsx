92421442ac246f0feb703f1070f582123749ee32
Fedorov Peter <fedorovpeter97@gmail.com>
2023-08-11 18:23:14 +0200

    Created states for title and description and onChange handlers

--- app/addTopic/page.jsx
+++ app/addTopic/page.jsx
@@ -1,13 +1,24 @@
+'use client';
+
+import { useState } from 'react';
+
 export default function AddTopic() {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+
   return (
     <form className="flex flex-col gap-3">
       <input
+        onChange={(e) => setTitle(e.target.value)}
+        value={title}
         className="border border-slate-500 px-8 py-2"
         type="text"
         placeholder="Topic title"
       />
 
       <input
+        onChange={(e) => setDescription(e.target.value)}
+        value={description}
         className="border border-slate-500 px-8 py-2"
         type="text"
         placeholder="Topic description"
